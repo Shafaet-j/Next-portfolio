69f7d0dac578bfe914d003d3144c997e5b851c6b
Shafaet-j <lbshaft01@gmail.com>
2023-08-21 15:49:16 +0600

    small fix

--- src/app/service/page.jsx
+++ src/app/service/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Service = () => {
-  return <div className=" text-center">Coming soon...</div>;
+  return <div className=" text-center text-3xl py-10">Coming soon...</div>;
 };
 
 export default Service;
